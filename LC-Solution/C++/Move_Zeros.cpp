a8d564d30f44c758352cdd043a4bedd64163ecd9
Yash Jangid <yashjangid0021@gmail.com>
2024-09-03 17:50:53 +0530

    Add files via upload

---
+++ LC-Solution/C++/Move_Zeros.cpp
@@ -0,0 +1,16 @@
+/*#My Leet Code id = im-yash21
+#Runtime:20 ms , Memory Usage: 22 MB */
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for(int i =  0 ;i<nums.size();i++){
+            if(nums[i]!=0){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            j++;
+            }
+        }
+    }
+};
